3e3dd2b6788df3485df991b0a26288a69dca6ba4
ricardoalonne <ricardo.alonne@gmail.com>
2022-06-11 22:22:01 -0500

    N5.UserPermissions.BackEnd: Se configuró las políticas de CORS en el backend.

--- WebApi/N5.UserPermissions.WebApi/Startup.cs
+++ WebApi/N5.UserPermissions.WebApi/Startup.cs
@@ -52,6 +52,8 @@ namespace N5.UserPermissions.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
